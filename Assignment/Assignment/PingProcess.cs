baf8b9dae9d41145e35f2cf3ae45e5abaa26de3b
Annika <astachowiak@ewu.edu>
2023-03-23 15:52:43 -0700

    Commit 3
    Fixed RunAsync

--- Assignment/Assignment/PingProcess.cs
+++ Assignment/Assignment/PingProcess.cs
@@ -45,6 +45,26 @@ public class PingProcess
         
     }
     ///////////////////////////////
+
+    async public Task<PingResult> RunAsync(params string[] hostNameOrAddresses)
+    {
+        StringBuilder stringBuilder = new();
+        ParallelQuery<Task<PingResult>>? all = hostNameOrAddresses.AsParallel().Select(async item =>
+        {
+            Task<PingResult> task = null!;
+            await task.WaitAsync(default(CancellationToken));
+            return task.Result;
+        });
+
+        await Task.WhenAll(all);
+        foreach (Task<PingResult> task in all)
+            stringBuilder.AppendLine(task.Result.StdOutput?.Trim());
+
+        int total = all.Aggregate(0, (total, item) => total + item.Result.ExitCode);
+        return new PingResult(total, stringBuilder.ToString().Trim());
+    }
+
+
     async public Task<PingResult> RunAsync(IEnumerable<string> hostNameOrAddresses, CancellationToken cancellationToken = default)
     {
         StringBuilder stringBuilder = new();
